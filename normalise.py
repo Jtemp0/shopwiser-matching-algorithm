58c8b9855ba72d85a60c7cc889b3b7eac06cf095
mionitsa <michael.ionitsa@gmail.com>
2026-03-17 22:08:45 +0000

    Add grocery vocabulary and brand categorization; refactor normalise.py to utilize new vocabulary definitions

--- normalise.py
+++ normalise.py
@@ -24,254 +24,13 @@ import unicodedata
 import warnings
 from typing import Tuple, List, Dict, Optional
 
+# Import variables from our new config file
+from grocery_vocab import (
+    SUPERMARKET_BRANDS, TIER_MAP, ATTRIBUTES, DESCRIPTORS, 
+    KNOWN_BRANDS, _VAPE_KEYWORDS
+)
+
 warnings.filterwarnings('ignore')
-
-# ============================================================
-# VOCABULARY & PATTERN DICTIONARIES
-# ============================================================
-
-SUPERMARKET_BRANDS: Dict[str, List[str]] = {
-    'Tesco': [
-        'Tesco Finest',
-        'Tesco Everyday Value',
-        'Tesco Free From',
-        'Tesco Organic',
-        'Hearty Food Co.',            # Tesco value ready-meal sub-brand
-        'Tesco',
-    ],
-    'ASDA': [
-        'ASDA Extra Special',
-        'Asda Extra Special',
-        'ASDA Smart Price',
-        'Asda Smart Price',
-        'ASDA Chosen By You',
-        'Asda Chosen By You',
-        'ASDA Free From',
-        'Asda Free From',
-        'COOK by ASDA',               # own-brand food line
-        'BAKE by ASDA',               # own-brand baking line
-        'The BAKERY at ASDA',         # own-brand bakery line
-        'ASDA',
-        'Asda',
-    ],
-    'Morrisons': [
-        'Morrisons The Best',
-        'Morrisons Savers',
-        'Morrisons Free From',
-        'Morrisons Organic',
-        'Morrisons Market Street',    # premium deli sub-brand
-        'Market Street',              # standalone form used in some names
-        'M Kitchen',
-        'Savers',                     # Morrisons value tier standalone
-        'Morrisons',
-    ],
-    'Sains': [
-        "Sainsbury's Taste the Difference",
-        "Sainsburys Taste the Difference",
-        'Taste the Difference',
-        "Sainsbury's Basics",
-        "Sainsburys Basics",
-        "Sainsbury's Free From",
-        "Sainsburys Free From",
-        "Sainsbury's SO Organic",
-        'SO Organic',
-        'JS',                         # abbreviated Sainsbury's prefix
-        "Sainsbury's",
-        "Sainsburys",
-    ],
-}
-
-# Map supermarket brand prefix → tier
-TIER_MAP = {
-    'finest': 'premium',
-    'extra special': 'premium',
-    'taste the difference': 'premium',
-    'the best': 'premium',
-    'market street': 'premium',      # Morrisons premium deli
-    'hearty food co': 'value',
-    'value': 'value',
-    'basics': 'value',
-    'savers': 'value',
-    'smart price': 'value',
-    'everyday value': 'value',
-}
-
-# Comprehensive list of known commercial brands
-KNOWN_BRANDS: List[str] = [
-    # Soft drinks, water, juice
-    'coca-cola', 'coca cola', 'pepsi', 'fanta', 'sprite', 'dr pepper', '7up', 'schweppes',
-    'ribena', 'robinsons', 'tropicana', 'innocent', 'capri sun', 'vimto', 'lucozade',
-    'red bull', 'monster', 'relentless', 'rockstar', 'boost',
-    'irn bru', 'irn-bru', 'oasis', 'rubicon', 'tango', 'ocean spray',
-    'san pellegrino', 'sanpellegrino', 'evian', 'volvic', 'buxton', 'highland spring',
-    'mountain dew', 'fever-tree', 'fever tree', 'britvic', 'j2o', 'fruit shoot',
-    'barr', 'shloer', 'belvoir', 'cawston press', 'the london essence co',
-    'long tail mixers', 'bottlegreen',
-
-    # Coffee & Tea
-    'nescafe', 'nescafé', 'twinings', 'tetley', 'pg tips', 'yorkshire tea', 'clipper',
-    'pukka', 'costa', 'starbucks', 'lavazza', 'illy', 'kenco', 'douwe egberts', 'tassimo',
-    'nespresso', 'dolce gusto', 'taylors of harrogate', 'taylors', 'cafe direct', 'cafedirect',
-    'maxwell house', 'l\'or', 'lor', 'leon', 'bird & blend', 'teapigs',
-
-    # Dairy, chilled & plant-based
-    'anchor', 'lurpak', 'flora', 'benecol', 'philadelphia', 'dairylea', 'babybel',
-    'muller', 'müller', 'yoplait', 'activia', 'danone', 'alpro', 'oatly',
-    'cathedral city', 'yeo valley', 'country life', 'clover', 'kerrygold',
-    'cravendale', 'elmlea', 'president', 'président', 'castello', 'onken',
-    'glenisk', 'violife', 'follow your heart', 'almond breeze', 'rude health',
-    'arla', 'delamere', 'rachel\'s', 'chobani', 'fage', 'galbani',
-    'light & free', 'yams', 'yamas',
-
-    # Breakfast & cereals
-    'weetabix', 'oatibix', 'alpen', 'jordans', 'ready brek', 'shreddies',
-    'cheerios', 'shredded wheat', 'special k', 'corn flakes', 'rice krispies',
-    'coco pops', 'frosties', 'nature valley', 'belvita', 'nakd', 'trek', 'kind',
-    'kellogg\'s', 'kelloggs', 'nestle', 'quaker', 'scotts', 'flahavan\'s',
-
-    # Bread, bakery, cakes
-    'warburtons', 'hovis', 'kingsmill', 'allinson', 'soreen', 'jus-rol',
-    'mcvitie\'s', 'mcvities', 'jacob\'s', 'jacobs', 'fox\'s', 'foxs',
-    'mr kipling', 'border biscuits', 'border', 'tunnock\'s', 'tunnocks',
-    'maryland', 'go ahead', 'go-ahead', 'lotus biscoff', 'biscoff',
-    'carr\'s', 'carrs', 'pop tarts', 'pop-tarts', 'graze', 'higgidy',
-    'miss molly\'s', 'finsbury food',
-
-    # Biscuits, sweets, chocolate
-    'cadbury', 'mars', 'snickers', 'galaxy', 'lindt', 'thorntons',
-    'green & black\'s', 'green and blacks', 'ferrero rocher', 'ferrero',
-    'toblerone', 'after eight', 'after eights', 'maltesers', 'm&ms', 'm&m\'s',
-    'bounty', 'milky way', 'twix', 'kit kat', 'kitkat',
-    'rowntree\'s', 'rowntrees', 'haribo', 'maynards bassetts', 'maynards', 'bassetts',
-    'swizzels', 'swizzels matlow', 'kinder', 'kinder bueno', 'werther\'s', 'werthers',
-    'tic tac', 'mentos', 'skittles', 'starburst', 'polo', 'chupa chups',
-    'reese\'s', 'reeses', 'hershey\'s', 'hersheys', 'terry\'s', 'terrys',
-    'kp', 'kp nuts', 'nobby\'s', 'nobbys', 'planters',
-    'gü', 'gu', 'gu puds',
-
-    # Crisps & savoury snacks
-    'walkers', 'pringles', 'doritos', 'mccoys', 'quavers', 'wotsits',
-    'kettle', 'kettle chips', 'tyrrells', 'seabrook', 'pom-bear', 'pombear',
-    'hula hoops', 'hulahoops', 'skips', 'discos', 'nik naks', 'niknaks',
-    'frazzles', 'monster munch', 'popchips', 'sunbites',
-    'propercorn', 'properchips', 'metcalfe\'s', 'metcalfes',
-    'snack a jacks', 'snackajacks', 'mini cheddars', 'cofresh', 'cheetos',
-
-    # Rice, grains, pasta, noodles
-    'ben\'s original', 'bens original', 'uncle ben\'s', 'uncle bens',
-    'tilda', 'laila', 'merchant gourmet', 'barilla', 'de cecco', 'dececco',
-    'napolina', 'batchelors', 'itsu', 'yutaka', 'amoy', 'nissin', 'kohinoor',
-    'dolmio', 'loyd grossman',
-
-    # Tins, jars, sauces, condiments
-    'heinz', 'branston', 'colman\'s', 'colmans', 'marmite', 'bovril', 'oxo', 'bisto',
-    'paxo', 'knorr', 'maggi', 'hellmann\'s', 'hellmanns',
-    'hp sauce', 'hp', 'lea & perrins', 'lea and perrins',
-    'frank\'s redhot', 'franks redhot', 'tabasco', 'encona', 'cholula',
-    'sriracha', 'flying goose', 'kikkoman', 'lee kum kee', 'blue dragon',
-    'old el paso', 'santa maria', 'geeta\'s', 'geetas', 'rajah',
-    'sharwood\'s', 'sharwoods', 'pataks', 'patak\'s', 'the spice tailor',
-    'schwartz', 'bart', 'maldon', 'saxa', 'sarson\'s', 'sarsons',
-    'haywards', 'whitworths', 'filippo berio', 'bertolli',
-    'green giant', 'cirio', 'mutti', 'sacla', 'fray bentos', 'princes',
-    'baxters', 'hartley\'s', 'hartleys', 'tiptree', 'robertson\'s', 'robertsons',
-    'duerr\'s', 'duerrs', 'bonne maman', 'rowse', 'nutella', 'sun-pat', 'sun pat',
-    'tracklements', 'encona', 'potts', 'lea & perrins',
-
-    # Frozen foods & ready meals
-    'mccain', 'goodfella\'s', 'goodfellas', 'dr oetker', 'dr. oetker',
-    'chicago town', 'rustlers', 'birdseye', 'birds eye',
-    'aunt bessie\'s', 'aunt bessies', 'findus',
-    'strong roots', 'cauldron', 'tofoo', 'the tofoo co',
-    'beyond meat', 'moving mountains', 'vivera', 'meatless farm', 'cook',
-
-    # Chilled meat, fish, pies
-    'wall\'s', 'walls', 'heck', 'herta', 'bernard matthews', 'mattessons',
-    'ginsters', 'pukka pies', 'quorn', 'linda mccartney', 'richmond',
-    'john west', 'youngs', 'young\'s', 'glenryck',
-
-    # Beer & Cider
-    'stella artois', 'stella', 'budweiser', 'bud', 'heineken', 'carlsberg',
-    'foster\'s', 'fosters', 'carling', 'kronenbourg', '1664',
-    'peroni', 'birra moretti', 'moretti', 'san miguel', 'estrella', 'estrella damm',
-    'guinness', 'john smith\'s', 'john smiths', 'boddingtons',
-    'brewdog', 'camden town', 'camden hells', 'beavertown', 'neck oil',
-    'strongbow', 'thatchers', 'kopparberg', 'magners', 'bulmers', 'old mout',
-    'corona', 'desperados', 'beck\'s', 'becks',
-    'old speckled hen', 'old crafty hen', 'spitfire', 'hobgoblin',
-    'birra peroni', 'bavaria', 'amstel', 'leffe',
-
-    # Spirits
-    'smirnoff', 'absolut', 'grey goose', 'glen\'s', 'glens',
-    'gordon\'s', 'gordons', 'bombay sapphire', 'tanqueray', 'hendrick\'s', 'hendricks',
-    'beefeater', 'edinburgh gin', 'brockmans', 'ciroc',
-    'jack daniel\'s', 'jack daniels', 'jim beam', 'maker\'s mark',
-    'jameson', 'famous grouse', 'bell\'s', 'bells', 'johnnie walker',
-    'glenfiddich', 'glenlivet', 'cardhu', 'highland park', 'macallan',
-    'captain morgan', 'bacardi', 'havana club', 'malibu', 'kraken',
-    'baileys', 'tia maria', 'kahlua', 'disaronno', 'southern comfort',
-    'pimms', 'pimm\'s', 'martini', 'aperol', 'campari',
-    'courvoisier', 'hennessy', 'courvoisier',
-    'warninks', 'advocaat',
-
-    # Wine & Champagne
-    'hardys', 'blossom hill', 'yellow tail', 'casillero del diablo',
-    'campo viejo', 'villa maria', 'oyster bay', '19 crimes', 'jam shed',
-    'barefoot', 'mcguigan', 'jacob\'s creek', 'jacobs creek',
-    'lindeman\'s', 'lindemans', 'wolf blass', 'errazuriz', 'trapiche',
-    'freixenet', 'moët', 'moet', 'veuve clicquot', 'bollinger', 'lanson',
-    'squealing pig', 'wollemi', 'giesen',
-
-    # Health, protein, wellness
-    'fulfil', 'phd nutrition', 'phd smart bar', 'the skinny food co',
-    'getpro', 'nakd', 'trek', 'kind', 'graze', 'nature valley',
-    'phd', 'optimum nutrition', 'myprotein', 'up&go', 'weetabix on the go',
-
-    # Baby & toddler
-    'cow & gate', 'cow and gate', 'aptamil', 'hipp', 'hipp organic',
-    'ella\'s kitchen', 'ellas kitchen', 'organix', 'kiddylicious',
-    'heinz by nature', 'piccolo', 'little freddie',
-
-    # Home baking
-    'tate & lyle', 'tate and lyle', 'silver spoon', 'billington\'s',
-    'allinson\'s', 'allinsons', 'homepride', 'betty crocker', 'doves farm',
-    'dr. oetker', 'silver spoon',
-
-    # Dairy-free
-    'oatly', 'alpro', 'provamil', 'koko', 'nutpods',
-
-    # Misc
-    'ambrosia', 'crosta & mollica', 'prymat', 'felix', 'soreen',
-    'express cuisine', 'staveley',
-]
-
-# Attributes (remove from name after detection)
-ATTRIBUTES: Dict[str, List[str]] = {
-    'organic': ['organic', 'bio', 'soil association'],
-    'free_from': ['gluten free', 'dairy free', 'lactose free', 'wheat free', 'free from'],
-    'diet': ['vegan', 'vegetarian', 'plant-based', 'plant based', 'low fat', 'reduced fat',
-             'fat free', 'sugar free', 'no added sugar', 'low sugar', 'light', 'lite'],
-    'ethical': ['fairtrade', 'fair trade', 'free range', 'freedom food', 'rspca assured'],
-    'preparation': ['ready to eat', 'ready to cook', 'ready meal', 'meal for one',
-                    'microwaveable', 'oven ready'],
-}
-
-# Descriptors (detected but NOT removed from name)
-DESCRIPTORS: Dict[str, List[str]] = {
-    'cooking_method': ['roasted', 'grilled', 'baked', 'fried', 'smoked', 'steamed',
-                       'poached', 'bbq', 'barbecue', 'chargrilled'],
-    'texture':        ['crunchy', 'crispy', 'smooth', 'chunky', 'creamy', 'thick', 'thin'],
-    'flavor':         ['mild', 'medium', 'hot', 'spicy', 'sweet', 'savoury', 'salty', 'tangy'],
-    'form':           ['sliced', 'diced', 'chopped', 'whole', 'halved', 'quartered', 'peeled',
-                       'shredded', 'grated', 'ground', 'crushed', 'powder', 'paste'],
-}
-
-# Keywords that indicate the mg value is nicotine strength, not product weight
-_VAPE_KEYWORDS = frozenset([
-    'e-liquid', 'e liquid', 'eliquid', 'vape', 'vaping', 'pod', 'pods',
-    'nicotine', 'tobacco', 'cigarette', 'e-cig', 'ecig', 'juul', 'elf bar',
-    'disposable', 'menthol e', 'nic salt',
-])
 
 # ============================================================
 # HELPER FUNCTIONS
@@ -305,6 +64,18 @@ def extract_supermarket_brand(name: str, supermarket: str) -> Tuple[Optional[str
             return prefix, remaining
     return None, name_str
 
+def extract_abv(name: str) -> Tuple[Optional[float], str]:
+    if not isinstance(name, str):
+        return None, ''
+    # Matches "4.5% ABV", "5%", "4.5 % vol"
+    pattern = r'(\d+(?:\.\d+)?)\s*%\s*(?:abv|vol)?\b'
+    match = re.search(pattern, name, re.IGNORECASE)
+    if match:
+        abv = float(match.group(1))
+        remaining = re.sub(pattern, '', name, flags=re.IGNORECASE)
+        remaining = re.sub(r'\s+', ' ', remaining).strip()
+        return abv, remaining
+    return None, name
 
 def extract_tier(supermarket_brand: Optional[str]) -> Tuple[Optional[str], Optional[str]]:
     if not supermarket_brand:
@@ -335,7 +106,7 @@ def extract_multipack(name: str) -> Tuple[Optional[int], Optional[str], str]:
         # "multipack 6"
         (r'multipack\s*(\d+)', 'multipack'),
         # Tea/sachets count: "80s" / "20 bags" at end of name or before unit
-        (r'\b(\d+)\s*(?:bags?|sachets?|tabs?)\b', 'bag_count'),
+        (r'\b(\d+)\s*(?:bags?|sachets?|tabs?|pods?|capsules?)\b', 'bag_count'),
         # "6 Pcs" / "12 pcs"
         (r'(\d+)\s*pcs?\b', 'count_pcs'),
     ]
@@ -509,6 +280,7 @@ def normalize_product_name(row: pd.Series) -> dict:
         'pack_pattern':         None,
         'unit_value':           None,
         'unit_type':            None,
+        'abv_percentage':       None,
         'unit_inferred':        False,
         'attributes_types':     [],
         'attributes_keywords':  [],
@@ -519,6 +291,9 @@ def normalize_product_name(row: pd.Series) -> dict:
 
     # Pre-processing
     current = clean_parenthetical_notes(original_name)
+
+    # Standardize ampersands to 'and' and normalize smart quotes
+    current = current.replace('&', ' and ').replace('’', "'")
 
     # Step 1: Supermarket brand extraction
     sm_brand, current = extract_supermarket_brand(current, supermarket)
@@ -538,6 +313,10 @@ def normalize_product_name(row: pd.Series) -> dict:
     unit_val, unit_type, current = extract_and_standardize_unit(current)
     result['unit_value'] = unit_val
     result['unit_type']  = unit_type
+
+    # Step 4.5: Extract Alcohol %
+    abv_val, current = extract_abv(current)
+    result['abv_percentage'] = abv_val
 
     # Step 4b: Fallback — infer unit from price if name extraction failed
     if unit_val is None:
